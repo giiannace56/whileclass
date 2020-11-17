d75da7afb1cb2d75cd46edfe3bfc56a6db7599ad
giovanni iannace <giovanniiannace56@gmail.com>
2020-11-17 16:42:12 -0300

    taboada feita

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace while_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Deseja saber a tabuada de que numero ?");
+            int resposta =int.Parse(Console.ReadLine());
+
+            int resultado ;
+
+            for (int contador = 10; contador > 0;contador--){
+                resultado = resposta * contador;
+                Console.WriteLine($"{resposta} * {contador} = {resultado}");
+            }
+        }
+    }
+}
